a3e0b3247c30760a351f81034ce2f61b74714795
Hiikey7 <kelvinramsiel@gmail.com>
2026-08-16 04:38:38 +0300

    Register onboarded and admin middleware aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\EnsureUserIsOnboarded;
+use App\Http\Middleware\EnsureUserIsPlatformAdmin;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
@@ -21,6 +23,11 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        $middleware->alias([
+            'onboarded' => EnsureUserIsOnboarded::class,
+            'admin' => EnsureUserIsPlatformAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
