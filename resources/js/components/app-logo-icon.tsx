e7072c7e4f76391d3e2e1efb824f464ac08bbb65
Hiikey7 <kelvinramsiel@gmail.com>
2026-08-27 08:50:15 +0300

    Fix logo SVG stroke-width JSX casing

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -13,21 +13,21 @@ export default function AppLogoIcon(props: SVGAttributes<SVGSVGElement>) {
     <path
         fill="rgb(21,119,118)"
         stroke="rgb(21,119,118)"
-        stroke-width="1"
+        strokeWidth="1"
         opacity="1"
         d="M 818 300 Q 836 298 842 309 L 845 323 Q 843 334 835 339 Q 830 343 818 342 L 807 335 Q 801 329 802 317 L 811 303 L 818 300 Z "
     />
     <path
         fill="rgb(194,119,80)"
         stroke="rgb(194,119,80)"
-        stroke-width="1"
+        strokeWidth="1"
         opacity="1" 
         d="M 590 185 L 704 285 L 817 185 L 1015 359 L 1014 418 L 984 392 L 983 393 L 983 471 L 984 544 L 983 563 L 984 575 L 983 588 L 984 590 L 983 593 L 874 593 L 874 548 L 934 548 L 936 546 L 935 412 L 936 349 L 817 245 L 739 314 L 747 324 L 853 417 L 824 450 L 703 346 L 585 450 L 555 417 L 670 315 L 590 246 L 473 348 L 473 546 L 474 548 L 535 548 L 535 593 L 425 593 L 425 394 L 422 393 L 394 418 L 394 358 L 590 185 Z "
     />
     <path
         fill="rgb(194,119,80)"
         stroke="rgb(194,119,80)"
-        stroke-width="1"
+        strokeWidth="1"
         opacity="1"
         d="M 658 468 L 750 468 L 750 593 L 658 593 L 658 468 Z M 725 522 L 725 540 L 735 540 L 736 536 L 736 522 L 725 522 Z "
     />
