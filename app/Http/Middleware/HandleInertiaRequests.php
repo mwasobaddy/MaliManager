28757498314fed02dd8c7b66dedb750b1de29bfc
Hiikey7 <kelvinramsiel@gmail.com>
2026-08-28 23:04:45 +0300

    Share centralUrl with the frontend

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -51,6 +51,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'centralUrl' => rtrim(config('app.url'), '/'),
             'auth' => [
                 'user' => $user,
                 'canRaiseMaintenance' => $user && $user->person_id
