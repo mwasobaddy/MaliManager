c870018208ee2720598865d0aa05215e57e64e44
Hiikey7 <kelvinramsiel@gmail.com>
2026-09-02 23:33:52 +0300

    Add StorePlatformExpense form request

---
+++ app/Http/Requests/Admin/StorePlatformExpenseRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Models\PlatformExpense;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StorePlatformExpenseRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'category' => ['required', 'string', Rule::in(PlatformExpense::CATEGORIES)],
+            'amount' => ['required', 'numeric', 'min:0'],
+            'currency' => ['required', 'string', 'size:3'],
+            'spent_on' => ['required', 'date'],
+            'vendor_name' => ['nullable', 'string', 'max:255'],
+            'notes' => ['nullable', 'string', 'max:2000'],
+        ];
+    }
+}
